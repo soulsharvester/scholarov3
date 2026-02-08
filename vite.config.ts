1bb3f20f4cdc4a0d5c6b8c6a0c50849a3ba494ed
Hamza Saif <hamzasaifworking@gmail.com>
2026-02-08 10:44:32 +0000

    Switch to root base for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/scholarov3/', // <-- Updated base URL
+  base: '/', // <-- Updated base URL
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
